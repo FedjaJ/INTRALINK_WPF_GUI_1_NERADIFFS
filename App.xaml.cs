36f4f3fc81db1627bcf488041103e4454857ca59
WiMAX <WiMAX@DESKTOP-RC28NK0>
2022-03-14 23:45:06 +0100

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace INTRALINK_WPF_GUI_1
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
